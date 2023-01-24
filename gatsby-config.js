957dff803634584ff42cd33e0c8e5c1f828ded0f
adueck <adueck@users.noreply.github.com>
2023-01-24 23:08:13 +0500

    fix rss

--- gatsby-config.js
+++ gatsby-config.js
@@ -83,8 +83,8 @@ module.exports = {
                 return Object.assign({}, node.frontmatter, {
                   description: node.excerpt,
                   date: node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + node.fields.slug,
+                  url: site.siteMetadata.siteUrl + "/blog" + node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + "/blog" + node.fields.slug,
                   custom_elements: [{ "content:encoded": node.html }],
                 })
               })
